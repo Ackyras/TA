539cc91f8d1877a9df024fe4ebbbac528dda4fdf
ackyras <ackyrasibarani@gmail.com>
2024-07-27 15:30:15 +0700

    chore; add burger men

--- spp_web/resources/views/components/dashboard/navbar.blade.php
+++ spp_web/resources/views/components/dashboard/navbar.blade.php
@@ -7,9 +7,6 @@
         <li class="nav-item d-none d-sm-inline-block">
             <a href="{{ route('dashboard.index') }}" class="nav-link">Home</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li>
     </ul>
     <ul class="ml-auto navbar-nav">
         <li class="nav-item">
